04ca022fd67d219b97c644ae9d68f5ff06b3a8bc
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2026-02-09 17:06:05 +0800

    refactor: create a utility tailwind class for the brand border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,34 @@ import {
 const bgColorClassNames = [
   {
     name: 'Black',
-    colors: ['bg-core-black-100', 'bg-core-black-200']
+    colors: ['bg-brand-black-100', 'bg-brand-black-200']
   },
   {
     name: 'Blue',
-    colors: ['bg-core-blue-100', 'bg-core-blue-200', 'bg-core-blue-300']
+    colors: ['bg-brand-blue-100', 'bg-brand-blue-200', 'bg-brand-blue-300']
   },
   {
     name: 'Green',
-    colors: ['bg-core-green-100', 'bg-core-green-200', 'bg-core-green-300']
+    colors: ['bg-brand-green-100', 'bg-brand-green-200', 'bg-brand-green-300']
   },
   {
     name: 'Yellow',
-    colors: ['bg-core-yellow-100', 'bg-core-yellow-200', 'bg-core-yellow-300']
+    colors: [
+      'bg-brand-yellow-100',
+      'bg-brand-yellow-200',
+      'bg-brand-yellow-300'
+    ]
   },
   {
     name: 'Red',
-    colors: ['bg-core-red-100', 'bg-core-red-200', 'bg-core-red-300']
+    colors: ['bg-brand-red-100', 'bg-brand-red-200', 'bg-brand-red-300']
   }
 ];
 
 export default function Home() {
   return (
     <div className='mx-auto flex min-h-screen w-full max-w-[1200px] flex-col items-center gap-20 py-20'>
-      <div className='flex h-fit w-full flex-col items-center rounded-2xl border-2 p-12 text-center'>
+      <div className='brand-border flex h-fit w-full flex-col items-center rounded-2xl p-12 text-center'>
         <TextDisplay className='mb-2'>Typography Scale</TextDisplay>
         <TextHeading className='mb-4'>Heading</TextHeading>
         <TextSubheading className='max-w-[500px]'>Subheading</TextSubheading>
@@ -40,8 +44,8 @@ export default function Home() {
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </TextBody>
       </div>
-      <div className='flex h-fit w-full flex-col items-center gap-12 rounded-2xl border-2 p-12'>
-        <TextHeading>Core Colors</TextHeading>
+      <div className='brand-border flex h-fit w-full flex-col items-center gap-12 rounded-2xl p-12'>
+        <TextHeading>brand Colors</TextHeading>
         <div className='flex flex-row gap-6'>
           {bgColorClassNames.map((color, index) => {
             return (
