c7062b51ca7eaccf80b3883fe9ab1a8e299be7f1
Derek P <pajarin.derek.b@gmail.com>
2026-02-12 15:05:10 +0800

    chore: test branch ruleset

--- src/app/_sections/hero/index.tsx
+++ src/app/_sections/hero/index.tsx
@@ -3,7 +3,7 @@ import { TextHeading } from '@/components/text';
 export default function Hero() {
   return (
     <div className='brand-border grid h-screen place-items-center border-x-0! px-4 py-20'>
-      <TextHeading>Derks was here</TextHeading>
+      <TextHeading>Hero Section</TextHeading>
     </div>
   );
 }
