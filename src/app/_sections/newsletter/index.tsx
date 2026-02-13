51c55709c3b4d6a7458bf7e05b7051ce937fddd8
Robby <jrcr2022-2647-26244@bicol-u.edu.ph>
2026-02-13 20:56:48 +0800

    fix(newsletter): remove unnecessary id attribute from newsletter section

--- src/app/_sections/newsletter/index.tsx
+++ src/app/_sections/newsletter/index.tsx
@@ -2,7 +2,7 @@ import { TextHeading } from '@/components/text';
 
 export default function Newsletter() {
   return (
-    <div id="contact" className='brand-border grid h-[700px] place-items-center border-x-0! border-t-0! px-4 py-20'>
+    <div className='brand-border grid h-[700px] place-items-center border-x-0! border-t-0! px-4 py-20'>
       <TextHeading>Newsletter</TextHeading>
     </div>
   );
